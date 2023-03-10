4cae9d09c5601b98007d563d382fee1bd87262a6
Abhishek Kumar Saini <abhii.saini102@gmail.com>
2023-03-10 22:09:51 +0530

    second commit

--- ExamSeatingArrangementSystem.java
+++ ExamSeatingArrangementSystem.java
@@ -1,11 +1,10 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+// import java.util.ArrayList;
+// import java.util.Scanner;
+import java.util.*;
 
 public class ExamSeatingArrangementSystem {
 
